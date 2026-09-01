b25b1c1e53e96dc184b3022ae846350550947b66
Michael Willingham <michael@willingham.io>
2026-09-01 00:29:43 -0500

    yolo

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,9 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-function fish_greeting
-    fastfetch -s break:os:kernel:shell:cpu:memory:gpu:disk:battery:break:colors
-end
-
 fish_add_path ~/.local/bin
 fish_add_path /opt/cuda/bin
 
