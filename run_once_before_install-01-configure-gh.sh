cd79e3570911b176ba2f14d30e21340bb331744e
Michael Willingham <michael@willingham.io>
2026-08-31 22:56:49 -0500

    Update GitHub CLI login command with options

--- run_once_before_install-01-configure-gh.sh
+++ run_once_before_install-01-configure-gh.sh
@@ -4,7 +4,7 @@
 shelly install standard github-cli -n
 
 # prompt user to log in to github cli
-gh auth login
+gh auth login -p ssh -h github.com -w --skip-ssh-key
 
 # setup git, maybe should move this to fish config so it just always runs, idk
 gh auth setup-git
