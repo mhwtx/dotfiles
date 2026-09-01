12e7fbaf111c861419e47b8a2b24ca1c089c57d7
Michael Willingham <michael@willingham.io>
2026-08-31 22:57:29 -0500

    Add TODO for conditional GitHub CLI login
    Added a TODO comment to check authentication status before prompting login.

--- run_once_before_install-01-configure-gh.sh
+++ run_once_before_install-01-configure-gh.sh
@@ -4,7 +4,6 @@
 shelly install standard github-cli -n
 
 # prompt user to log in to github cli
+# TODO should only do this if gh auth status says we are not logged in
 gh auth login -p ssh -h github.com -w --skip-ssh-key
-
-# setup git, maybe should move this to fish config so it just always runs, idk
 gh auth setup-git
