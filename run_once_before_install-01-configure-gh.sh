617d2397a85e4d0397ab1973bd1a96f34740c424
Michael Willingham <michael@willingham.io>
2026-08-31 22:58:34 -0500

    Modify GitHub CLI login to check auth status
    Add check for GitHub CLI authentication before login.

--- run_once_before_install-01-configure-gh.sh
+++ run_once_before_install-01-configure-gh.sh
@@ -4,6 +4,9 @@
 shelly install standard github-cli -n
 
 # prompt user to log in to github cli
-# TODO should only do this if gh auth status says we are not logged in
-gh auth login -p ssh -h github.com -w --skip-ssh-key
+if ! gh auth status >/dev/null 2>&1; then
+  gh auth login -p ssh -h github.com -w --skip-ssh-key
+fi
+
+# set up git authentication
 gh auth setup-git
