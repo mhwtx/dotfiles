1cb8f0ecbf1e95d1b95f1f3775ca93e8c53d3fcc
Michael Willingham <michael@willingham.io>
2026-09-07 17:54:26 -0500

    yolo

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -23,8 +23,5 @@ abbr -a nsmi nvidia-smi
 abbr -a t temporal
 abbr -a tf tofu
 
-# Key bindings
-# Tide's async prompt is `fish -c`, which does not init bindings. If
-# fish_key_bindings is unset, emacs bind mode `default` is treated as
-# vi-normal and the prompt character becomes ❮ instead of ❯.
-set --global fish_key_bindings fish_default_key_bindings
+# OrbStack
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
