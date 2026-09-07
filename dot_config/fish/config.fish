9b41f89879e425a228775f17bb94121eee1d3cb4
Michael Willingham <michael@Michaels-MacBook-Pro.local>
2026-09-07 17:50:54 -0500

    yolo

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,18 +1,30 @@
-# CachyOS
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+# Interactive config
+if status is-interactive
+    fastfetch
+    zoxide init fish --cmd cd | source
+end
 
-# Path
+# Paths
 fish_add_path ~/.local/bin
+fish_add_path ~/.lmstudio/bin
 fish_add_path /opt/cuda/bin
 
 # Environment
-set -gx EDITOR helix
+set -gx EDITOR hx
+
+# Abbreviations
+abbr -a c chezmoi
+abbr -a cls clear
+abbr -a d docker
+abbr -a e exit
+abbr -a ff fastfetch
+abbr -a k kubectl
+abbr -a nsmi nvidia-smi
+abbr -a t temporal
+abbr -a tf tofu
 
 # Key bindings
 # Tide's async prompt is `fish -c`, which does not init bindings. If
 # fish_key_bindings is unset, emacs bind mode `default` is treated as
 # vi-normal and the prompt character becomes ❮ instead of ❯.
 set --global fish_key_bindings fish_default_key_bindings
-
-# Tools
-zoxide init fish --cmd cd | source
