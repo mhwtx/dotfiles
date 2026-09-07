cb424727399c70adb64c03927096d7c5eaad258f
Michael Willingham <michael@willingham.io>
2026-09-07 16:19:43 -0500

    Delete dot_config/fish/functions directory

--- dot_config/fish/functions/cls.fish
+++
@@ -1,3 +0,0 @@
-function cls --wraps=cler --wraps=clear --description 'alias cls=clear'
-    clear $argv
-end
--- dot_config/fish/functions/gyolo.fish
+++
@@ -1,3 +0,0 @@
-function gyolo --wraps="git add * && git commit -m 'yolo' && git push" --description "alias gyolo=git add * && git commit -m 'yolo' && git push"
-    git add * && git commit -m 'yolo' && git push $argv
-end
