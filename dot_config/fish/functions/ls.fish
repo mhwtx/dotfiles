ccf19f52ab4f5183ed4f1eb1165f0e45a1dbe6e2
Michael Willingham <michael@willingham.io>
2026-09-07 19:22:46 -0500

    yolo

--- dot_config/fish/functions/ls.fish
+++ dot_config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='eza -lh --icons' --description 'alias ls=eza -lh --icons'
-    eza -lh --icons $argv
+function ls --wraps='eza -lh --icons auto' --description 'alias ls=eza -lh --icons auto'
+    eza -lh --icons auto $argv
 end
